2abcdbd825a83d36c3500e06db141f029879a42f
lucas <lucascabraldevv@gmail.com>
2022-02-14 16:28:23 -0300

    DiscoverApi to get movies created.

---
+++ app/src/main/java/com/lucascabral/moviesapp/data/DiscoverApi.kt
@@ -0,0 +1,14 @@
+package com.lucascabral.moviesapp.data
+
+import com.lucascabral.moviesapp.data.model.BasePaginationRemote
+import com.lucascabral.moviesapp.data.model.MovieRemote
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface DiscoverApi {
+
+    @GET("discover/movie")
+    suspend fun getMovies(
+        @Query("api_key") token: String
+    ) : BasePaginationRemote<List<MovieRemote>>
+}
